fcfa30774a8d5a5b700c0a58e1b0b82682987409
Roger1492 <rogershen1492@gmail.com>
2017-01-31 09:28:20 +0800

    add qinnu

---
+++ Websites-PageHome/qiniu/js/script.js
@@ -0,0 +1,24 @@
+setInterval(bigSlider, 3000);
+setInterval(smallSlider, 3000);
+var wrapshow = document.getElementsByClassName("wrap-show")[0];
+var smallshow = document.getElementsByClassName("small-show")[0];
+var number1 = 0;
+var number2 = 0;
+
+function bigSlider() {
+    number1 += 100;
+    wrapshow.style.transform = "translate(-" + number1 + "vw)";
+    wrapshow.style.transition = "all 1s";
+    if (number1 == 300) {
+        number1 = -100;
+    }
+}
+
+function smallSlider() {
+    number2 += 400;
+    smallshow.style.transform = "translate(-" + number2 + "px)";
+    smallshow.style.transition = "all 1s";
+    if (number2 == 1200) {
+        number2 = -400;
+    }
+}
