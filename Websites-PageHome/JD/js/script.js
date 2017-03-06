4ff26d6015cfc5f9b09ca0c190d7562ef1a4e58e
Roger1492 <rogershen1492@gmail.com>
2017-03-06 18:15:40 +0800

    add some JD code

--- Websites-PageHome/JD/js/script.js
+++ Websites-PageHome/JD/js/script.js
@@ -1,8 +1,17 @@
+// window.onload = function(){
+// 	var jd_h_location = document.getElementById("jd-h-location");
+// 	var location_text = document.getElementsByTagName("table")[0];
+// 	location_text.addEventListener("click", function(e){
+// 		jd_h_location.innerHTML = e.target.innerHTML;
+// 		// e.stopPropagation();
+// 	}, false)
+// }
 window.onload = function(){
-	var jd_h_location = document.getElementById("jd-h-location");
-	var location_text = document.getElementsByTagName("table")[0];
-	location_text.addEventListener("click", function(e){
-		jd_h_location.innerHTML = e.target.innerHTML;
-		// e.stopPropagation();
-	}, false)
+	var jd_h_location = document.getElementById("jd-h-location")
+	var location_text = document.getElementsByTagName("table")[0].getElementsByTagName("td");
+	for(var i = 0; i < location_text.length; i++){
+		location_text[i].addEventListener("click", function(){
+			jd_h_location.innerHTML = this.innerHTML;
+		}, false)
+	}
 }
